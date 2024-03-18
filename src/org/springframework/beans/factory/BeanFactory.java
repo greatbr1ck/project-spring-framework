024ca84b7bf9695d9e9c803c96676cd786f58821
Светлицкий Михаил <svetlitskii.ma@phystech.edu>
2024-03-18 22:53:28 +0300

    Add BeanFactory class

---
+++ src/org/springframework/beans/factory/BeanFactory.java
@@ -0,0 +1,61 @@
+package org.springframework.beans.factory;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.stereotype.Component;
+
+public class BeanFactory {
+
+  private Map<String, Object> singletons = new HashMap<>();
+
+  public Object getBean(String beanName) {
+    return singletons.get(beanName);
+  }
+
+  public void instantiate(String basePackage)
+      throws IOException, URISyntaxException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    String path = basePackage.replace('.', '/');
+    Enumeration<URL> resources = ClassLoader.getSystemClassLoader().getResources(path);
+    while (resources.hasMoreElements()) {
+      URL resource = resources.nextElement();
+      System.out.println(resource);
+      File file = new File(resource.toURI());
+      String[] fileNames = file.list();
+      for (var fileName : fileNames) {
+        if (fileName.endsWith(".class")) {
+          String className = fileName.substring(0, fileName.lastIndexOf("."));
+          Class classObject = Class.forName(basePackage + "." + className);
+          if (classObject.isAnnotationPresent(Component.class)) {
+            System.out.println("Component: " + classObject);
+            Object instance = classObject.newInstance();
+            String beanName = className.substring(0, 1).toLowerCase() + className.substring(1);
+            singletons.put(beanName, instance);
+          }
+        }
+      }
+    }
+  }
+
+  public void populateProperties() throws IllegalAccessException {
+    System.out.println("==populateProperties==");
+    for (Object object : singletons.values()) {
+      for (Field field : object.getClass().getDeclaredFields()) {
+        if (field.isAnnotationPresent(Autowired.class)) {
+          for (Object dependency : singletons.values()) {
+            if (dependency.getClass().equals(field.getType())) {
+              field.setAccessible(true);
+              field.set(object, dependency);
+            }
+          }
+        }
+      }
+    }
+  }
+}
