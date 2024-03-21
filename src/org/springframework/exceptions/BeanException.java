9c782e612cba5addfe1d7ef82babdb419225d440
Свиридова Надежда <sviridova.nd@phystech.edu>
2024-03-21 16:27:52 +0300

    Add BeanException

---
+++ src/org/springframework/exceptions/BeanException.java
@@ -0,0 +1,8 @@
+package org.springframework.exceptions;
+
+public class BeanException extends Exception {
+  public BeanException() {
+    super("Error! There must be only 1 Bean from the Class");
+  }
+
+}
