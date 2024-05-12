54e173cd6109ecd3c64809edaccd3c0d744b455b
Светлицкий Михаил <svetlitskii.ma@phystech.edu>
2024-05-12 23:22:49 +0300

    add test scheduled methods

--- src/test/App.java
+++ src/test/App.java
@@ -2,6 +2,7 @@ package test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.exceptions.BeanException;
+import org.springframework.exceptions.ScheduledMethodException;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -9,12 +10,13 @@ import java.net.URISyntaxException;
 public class App {
 
     public static void main(String[] args)
-            throws IOException, URISyntaxException, ReflectiveOperationException, BeanException {
+            throws IOException, URISyntaxException, ReflectiveOperationException, BeanException, InterruptedException, ScheduledMethodException {
 
         ApplicationContext applicationContext = new ApplicationContext(
                 MyApplicationContextConfiguration.class);
-        ProductService productService = (ProductService) applicationContext.getBeanFactory()
-                .getBean("test.ProductService");
-        System.out.println(productService.getPromotionsService().getId());
+        ProductService productService = (ProductService) applicationContext.getBeanFactory().getBean("test.ProductService");
+        Thread.sleep(5000);
+        System.out.println("close");
+        applicationContext.close();
     }
 }
